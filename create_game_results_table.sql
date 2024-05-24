c60d4c1acdf6e44beb546dd4a82391b895f9372b
Gregory-Arbouzov <greg1703@utexas.edu>
2024-05-24 08:57:28 -0500

    PostgreSQL Connection

---
+++ create_game_results_table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS game_results (
+    id VARCHAR(14) PRIMARY KEY,
+    vistor TEXT NOT NULL,
+    home TEXT NOT NULL,
+    dates INT NOT NULL,
+    visitor_score INT NOT NULL,
+    home_score INT NOT NULL
+);

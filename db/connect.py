c9244cde5227b1ae3941020753c6b42574a82405
Gregory-Arbouzov <greg1703@utexas.edu>
2025-01-29 09:26:33 -0600

    connect update 2

--- db/connect.py
+++ db/connect.py
@@ -11,18 +11,21 @@ postgres_config = DatabaseConfig(filename = 'database.ini', section = 'postgresq
 
 class CreateSqlTables:
     def __init__(self, file, config):
-        connection = None
+        self.connection = None
         self.file = file
         self.config = config
-        
+
     try:
         params = self.config
 
         print('connecting to postgresql database...')
         connection = psycopg2.connect(**params)
+
+        print("connection successful / task complete")
        
         # create cursor
         cursor = connection.cursor()
+
         
         # add all sql create table commands here
         #cursor.execute(open('create_game_results_table.sql', 'r').read())

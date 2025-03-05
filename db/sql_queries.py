0bea870ae3e8f3c6624d8becb69a3544e9d23818
Gregory-Arbouzov <greg1703@utexas.edu>
2025-03-05 09:00:59 -0600

    script updates

--- db/sql_queries.py
+++ db/sql_queries.py
@@ -3,6 +3,8 @@
 
 get_all_game_results_dates_already_in_db = """SELECT DISTINCT(dates) FROM game_results;"""
 
+get_all_scrape_attempt_dates = """SELECT DISTINCT(dates) FROM game_dates_already_scraped;"""
+
 get_all_yearly_team_stats_already_in_db = """SELECT DISTINCT(id) FROM yearly_team_per_game_stats;"""
 
 get_all_yearly_opponent_stats_already_in_db = """SELECT DISTINCT(id) FROM yearly_opponent_per_game_stats;"""
--- scraping/all_game_results_scraping_script.py
+++ scraping/all_game_results_scraping_script.py
@@ -1,20 +1,33 @@
 from scraping import helpers
-#import db
 from db import config, all_db_stuff, sql_queries
-#from db import All_db_stuff
-#from db import sql_queries
 
 import psycopg2
 from configparser import ConfigParser
 
-#db.connect.DatabaseConfig(filename = 'database.ini', section = 'postgresql')
+valid_game_days = helpers.valid_game_days
 
 postgres_config = config.DatabaseConfig(filename = 'nba_stats_tracking/db/database.ini', section = 'postgresql')
     
 db1 = all_db_stuff.Database(postgres_config.get_config_params())
 db1.connect()
-#print(db1.get_data(sql_queries.get_all_game_results_dates_already_in_db))
-#db1.create_table('db/create_table_files/create_game_dates_already_scraped_table.sql')
+
+days_with_results_raw = db1.get_data(sql_queries.get_all_game_results_dates_already_in_db)
+days_with_results = [x[0] for x in days_with_results_raw]
+
+#print(days_with_results)
+
+for day in valid_game_days[:100]:
+    if day in days_with_results:
+        try:
+            print('attempting to insert ' + str(day))
+            print("""INSERT INTO game_dates_already_scraped (id, dates) VALUES ({0}, {1}); """.format(tuple([day, day])))
+            #db1.insert_data(("""INSERT INTO game_dates_already_scraped (id, dates) VALUES (%d, %d); """, (day, day)))
+        except psycopg2.Error as error:
+            print('Failed to insert data:', error)
+    else:
+        print(str(day) + ' has no data')
+
+print(db1.get_data(sql_queries.get_all_scrape_attempt_dates))
 
 db1.close()
 
